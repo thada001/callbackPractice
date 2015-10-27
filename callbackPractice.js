1287dc31b88bbfa8609e0ddebd85c083aff37dc6
Thomas Adair <thomascadair@gmail.com>
2015-10-27 16:08:08 -0500

    done callbacks

--- callbackPractice.js
+++ callbackPractice.js
@@ -24,7 +24,10 @@ and what you should write is the sayHi function that makes the code above work,
 
 
 
-  //Code Here for first
+var first = function(arr, cb) {
+  var firstName = arr[0];
+  cb(firstName);
+}
   
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -39,7 +42,10 @@ first(names, function(firstName){
 
 
 
-  //Code Here for last
+var last = function(arr, cb) {
+  var lastName = arr[arr.length-1];
+  cb(lastName);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
@@ -57,7 +63,10 @@ last(names, function(lastName){
 
 
 
-  //Code Here for multiply
+var multiply = function(a, b, cb) {
+  a = a*b;
+  cb(a);
+}
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -73,7 +82,15 @@ multiply(4, 3, function(answer){
 
 
 
-  //Code Here for contains
+var contains = function(arr, name, cb) {
+  var result;
+  if (arr.indexOf(name) === -1) {
+    result = false;
+  }else {
+    result = true;
+  }
+  cb(result);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(result){
@@ -93,7 +110,15 @@ contains(names, 'Colt', function(result){
 
 
 
-    //Code Here for uniq
+var uniq = function(arr, cb) {
+  var newArray = [arr[0]];
+  for (var i = 1; i < arr.length; i++) {
+    if (newArray.indexOf(arr[i]) === -1) {
+      newArray.push(arr[i]);
+    }
+  }
+  cb(newArray);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
@@ -109,7 +134,14 @@ uniq(names, function(uniqArr){
 
 
 
-    //Code Here for each
+var each = function(arr, cb) {
+  var ind, item;
+  for (var i = 0; i < arr.length; i++) {
+    ind = i;
+    item = arr[i];
+    cb(item, ind)
+  }
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
@@ -126,7 +158,15 @@ each(names, function(item, indice){
 
 
 
- //code here for getUserById
+var getUserById = function(arr, id, cb){
+  var user;
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].id === '16t') {
+      user = arr[i];
+    }
+  }
+  cb(user);
+}
 
 var users = [
   {
